67c37fe5ac387f6ecc8caa675cbe49ad6c697eef
heshan <5373827@qq.com>
2020-03-03 14:42:24 +0800

    l101

---
+++ src/main/java/l101/Solution.java
@@ -0,0 +1,30 @@
+package l101;
+
+import shared.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/3
+ */
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left != null && right != null) {
+            return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        }
+        return false;
+    }
+}
