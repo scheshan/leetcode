c005a0859f86fe3dd27aa5ffe7f6fdf291b947b8
heshan <5373827@qq.com>
2020-03-03 11:42:52 +0800

    l104

---
+++ src/main/java/l104/Solution.java
@@ -0,0 +1,20 @@
+package l104;
+
+import shared.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/3
+ */
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
