ec28d7f3b5b16e6b7ac7baf790439a6f5d983c3b
heshan <5373827@qq.com>
2020-03-06 20:57:42 +0800

    m68_2

---
+++ src/main/java/m68_2/Solution.java
@@ -0,0 +1,28 @@
+package m68_2;
+
+import shared.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/6
+ */
+public class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        } else if (right == null) {
+            return left;
+        } else {
+            return root;
+        }
+    }
+}
