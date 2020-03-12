1c6f65037cf3f5e229ef83113c66dbbf813ac79d
heshan <5373827@qq.com>
2020-03-12 17:20:40 +0800

    l1071

---
+++ src/main/java/l1071/Solution.java
@@ -0,0 +1,22 @@
+package l1071;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/12
+ */
+public class Solution {
+
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+    }
+
+    private int gcd(int a, int b) {
+        return b == 0? a: gcd(b, a % b);
+    }
+}
