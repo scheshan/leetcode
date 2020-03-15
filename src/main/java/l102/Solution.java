e8ad02982af0bf7616768de68469e3d10f38ab53
scheshan <5373827@qq.com>
2020-03-15 18:42:25 +0800

    l102

---
+++ src/main/java/l102/Solution.java
@@ -0,0 +1,42 @@
+package l102;
+
+import shared.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/15
+ */
+public class Solution {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        List<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            List<TreeNode> list = queue;
+            queue = new LinkedList<>();
+
+            List<Integer> nums = new LinkedList<>();
+
+            for (TreeNode node : list) {
+                nums.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            result.add(nums);
+        }
+
+        return result;
+    }
+}
