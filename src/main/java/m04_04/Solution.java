bb8f58d0601155e00b3a5f930e11cacd695ea2ba
scheshan <5373827@qq.com>
2020-03-15 16:56:41 +0800

    m04_04

---
+++ src/main/java/m04_04/Solution.java
@@ -0,0 +1,35 @@
+package m04_04;
+
+import shared.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/15
+ */
+public class Solution {
+
+    private boolean result = true;
+
+    public boolean isBalanced(TreeNode root) {
+        maxHeight(root);
+
+        return result;
+    }
+
+    private int maxHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = maxHeight(node.left);
+        int right = maxHeight(node.right);
+
+        if (Math.abs(left - right) > 1) {
+            result = false;
+        }
+
+        return Math.max(left, right) + 1;
+    }
+}
