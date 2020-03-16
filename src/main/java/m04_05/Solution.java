9671387ee7166f6bbf9041788fef19331dc9fa68
heshan <5373827@qq.com>
2020-03-16 16:22:40 +0800

    m04_05

---
+++ src/main/java/m04_05/Solution.java
@@ -0,0 +1,31 @@
+package m04_05;
+
+import shared.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/16
+ */
+public class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode node, Integer min, Integer max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (min != null && node.val <= min) {
+            return false;
+        }
+        if (max != null && node.val >= max) {
+            return false;
+        }
+
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+    }
+}
