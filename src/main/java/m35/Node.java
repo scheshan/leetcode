f2d25bc01adadc3b09ad30b0a78b7d8898a8561d
heshan <5373827@qq.com>
2020-03-18 12:10:15 +0800

    m35

---
+++ src/main/java/m35/Node.java
@@ -0,0 +1,19 @@
+package m35;
+
+/**
+ * Node
+ *
+ * @author heshan
+ * @date 2020/3/18
+ */
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
---
+++ src/main/java/m35/Solution.java
@@ -0,0 +1,35 @@
+package m35;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/18
+ */
+public class Solution {
+
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> map = new HashMap<>();
+
+        return clone(map, head);
+    }
+
+    private Node clone(Map<Node, Node> map, Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        if (map.containsKey(node)) {
+            return map.get(node);
+        } else {
+            Node clone = new Node(node.val);
+            map.put(node, clone);
+            clone.random = clone(map, node.random);
+            clone.next = clone(map, node.next);
+            return clone;
+        }
+    }
+}
