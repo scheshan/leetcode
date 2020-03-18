7d36054f8f4b4ed8c6cc3a1d2c3848a78f51f60b
heshan <5373827@qq.com>
2020-03-18 16:25:55 +0800

    m02_05

---
+++ src/main/java/m02_05/Solution.java
@@ -0,0 +1,62 @@
+package m02_05;
+
+import shared.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/18
+ */
+public class Solution {
+
+    private boolean b;
+
+    private ListNode head;
+
+    private ListNode tail;
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        while (l1 != null && l2 != null) {
+            newNode(l1.val + l2.val);
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        while (l1 != null) {
+            newNode(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            newNode(l2.val);
+            l2 = l2.next;
+        }
+
+        if (b) {
+            newNode(0);
+        }
+
+        return head;
+    }
+
+    private void newNode(int v) {
+        if (b) {
+            v++;
+        }
+        if (v >= 10) {
+            b = true;
+            v = v - 10;
+        } else {
+            b = false;
+        }
+
+        ListNode node = new ListNode(v);
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+    }
+}
