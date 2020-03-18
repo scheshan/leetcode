8c0375d8c2b15239221f0dffdcd810ae3933f721
heshan <5373827@qq.com>
2020-03-18 12:00:55 +0800

    m02_03

---
+++ src/main/java/m02_03/Solution.java
@@ -0,0 +1,17 @@
+package m02_03;
+
+import shared.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/18
+ */
+public class Solution {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
