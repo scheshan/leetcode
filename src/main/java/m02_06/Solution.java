2daf1bd178e68de557789a80fec53087a9691b0f
scheshan <5373827@qq.com>
2020-03-20 21:22:11 +0800

    m02_06

---
+++ src/main/java/m02_06/Solution.java
@@ -0,0 +1,44 @@
+package m02_06;
+
+import shared.ListNode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/20
+ */
+public class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        Deque<ListNode> deque = new LinkedList<>();
+        ListNode node = head;
+        while (node != null) {
+            deque.addFirst(node);
+            node = node.next;
+        }
+
+        ListNode left = head;
+        while (left != null) {
+            ListNode right = deque.remove();
+
+            if (left == right) {
+                return true;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+
+            left = left.next;
+        }
+
+        return true;
+    }
+}
