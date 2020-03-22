36fdb1416a55b2f15ada509bbbe098d7f3de1e39
scheshan <5373827@qq.com>
2020-03-22 13:25:29 +0800

    l268

---
+++ src/main/java/l268/Solution.java
@@ -0,0 +1,24 @@
+package l268;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/22
+ */
+public class Solution {
+
+    public int missingNumber(int[] nums) {
+        int res = 0;
+
+        for (int i = 0; i < nums.length + 1; i++) {
+            res = res ^ i;
+
+            if (i < nums.length) {
+                res = res ^ nums[i];
+            }
+        }
+
+        return res;
+    }
+}
