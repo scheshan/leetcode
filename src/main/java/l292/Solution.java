dcc6fce5e004924830cce14910832ef970bb62a1
scheshan <5373827@qq.com>
2020-03-22 19:23:11 +0800

    l292

---
+++ src/main/java/l292/Solution.java
@@ -0,0 +1,14 @@
+package l292;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/22
+ */
+public class Solution {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
