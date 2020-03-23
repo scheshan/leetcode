d4e6bfa2546f116f6706f9ea317fa056f89a9c2b
heshan <5373827@qq.com>
2020-03-23 18:15:47 +0800

    l387

---
+++ src/main/java/l387/Solution.java
@@ -0,0 +1,33 @@
+package l387;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/23
+ */
+public class Solution {
+
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.compute(s.charAt(i), (k, v) -> {
+                if (v == null) {
+                    v = 0;
+                }
+                return ++v;
+            });
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
