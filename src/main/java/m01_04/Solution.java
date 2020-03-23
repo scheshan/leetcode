ecd7c25567288017a60fea9989a7ea7afd3e790e
heshan <5373827@qq.com>
2020-03-23 18:05:24 +0800

    m01_04

---
+++ src/main/java/m01_04/Solution.java
@@ -0,0 +1,27 @@
+package m01_04;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/23
+ */
+public class Solution {
+
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (set.contains(s.charAt(i))) {
+                set.remove(s.charAt(i));
+            } else {
+                set.add(s.charAt(i));
+            }
+        }
+
+        return set.size() <= 1;
+    }
+}
