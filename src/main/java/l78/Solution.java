3e60cab8fe9de09e1e237a1ecfc8914b23f2a9ef
heshan <5373827@qq.com>
2020-03-23 19:55:33 +0800

    l78

---
+++ src/main/java/l78/Solution.java
@@ -0,0 +1,37 @@
+package l78;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/23
+ */
+public class Solution {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+
+        perm(res, new LinkedList<>(), nums, -1);
+
+        return res;
+    }
+
+    private void perm(List<List<Integer>> res, LinkedList<Integer> list, int[] nums, int i) {
+        if (i >= 0) {
+            list.add(nums[i]);
+        }
+
+        res.add(new ArrayList<>(list));
+        for (int j = i + 1; j < nums.length; j++) {
+            perm(res, list, nums, j);
+        }
+
+        if (i >= 0) {
+            list.removeLast();
+        }
+    }
+}
