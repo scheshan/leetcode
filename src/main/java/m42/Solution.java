7ef62ac8eb42c426b2764e27c1bce6a10d592fbb
heshan <5373827@qq.com>
2020-03-27 12:48:07 +0800

    m42

---
+++ src/main/java/m42/Solution.java
@@ -0,0 +1,27 @@
+package m42;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/3/27
+ */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+            res = Math.max(res, nums[i]);
+
+            if (nums[i] < 0) {
+                nums[i] = 0;
+            }
+        }
+
+        return res;
+    }
+}
