19cbd04e30260aa9682ffe636eef9ff5798c1771
heshan <5373827@qq.com>
2020-03-29 22:29:32 +0800

    l209

--- src/main/java/l209/Solution.java
+++ src/main/java/l209/Solution.java
@@ -9,23 +9,19 @@ package l209;
 public class Solution {
 
     public int minSubArrayLen(int s, int[] nums) {
-        int left = -1;
-        int right = -1;
+        int left = 0;
+        int right = 0;
 
         int res = Integer.MAX_VALUE;
         int cur = 0;
 
-        while (right < nums.length - 1) {
-            cur += nums[++right];
+        while (right < nums.length) {
+            cur += nums[right++];
 
-            while (cur < s && right < nums.length - 1) {
-                cur += nums[++right];
-            }
-
-            while (cur >= s && left < right) {
+            while (cur >= s) {
                 res = Math.min(right - left, res);
 
-                cur -= nums[++left];
+                cur -= nums[left++];
             }
         }
 
