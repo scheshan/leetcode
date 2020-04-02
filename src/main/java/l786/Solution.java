86fb7e1297676f5a08b6776ae58bccdd32b5a721
heshan <5373827@qq.com>
2020-04-02 14:03:06 +0800

    l786

--- src/main/java/l786/Solution.java
+++ src/main/java/l786/Solution.java
@@ -11,44 +11,29 @@ import java.util.PriorityQueue;
  */
 public class Solution {
 
-    private class Pair {
-
-        int p;
-
-        int q;
-
-        double value;
-
-        public Pair(int p, int q) {
-            this.p = p;
-            this.q = q;
-
-            value = 1.0 * p / q;
-        }
-    }
-
     public int[] kthSmallestPrimeFraction(int[] A, int K) {
-        PriorityQueue<Pair> queue = new PriorityQueue<>(K, new Comparator<Pair>() {
+        PriorityQueue<int[]> queue = new PriorityQueue<>(K, new Comparator<int[]>() {
             @Override
-            public int compare(Pair o1, Pair o2) {
-                return Double.compare(o2.value, o1.value);
+            public int compare(int[] o1, int[] o2) {
+                return Double.compare(A[o1[0]] * 1.0 / A[o1[1]], A[o2[0]] * 1.0 / A[o2[1]]);
             }
         });
 
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 1; j < A.length; j++) {
-                Pair p = new Pair(A[i], A[j]);
-                if (queue.size() < K) {
-                    queue.add(p);
-                } else {
-                    if (p.value < queue.peek().value) {
-                        queue.remove();
-                        queue.add(p);
-                    }
-                }
-            }
+        for (int i = 1; i < A.length; i++) {
+            queue.add(new int[]{0, i});
         }
 
-        return new int[]{queue.peek().p, queue.peek().q};
+        for (int i = 0; i < K - 1; i++) {
+            int[] item = queue.remove();
+            if (A[item[0] + 1] < A[item[1]]) {
+                item[0]++;
+                queue.add(item);
+            }
+        }
+        return new int[]{A[queue.peek()[0]], A[queue.peek()[1]]};
+    }
+
+    public static void main(String[] args) {
+        new Solution().kthSmallestPrimeFraction(new int[]{1, 2, 3, 5}, 3);
     }
 }
