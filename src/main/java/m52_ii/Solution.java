33d7a6096b209c849e494c829831dc4181bde80c
heshan <5373827@qq.com>
2020-04-07 14:41:38 +0800

    m52_ii

---
+++ src/main/java/m52_ii/Solution.java
@@ -0,0 +1,23 @@
+package m52_ii;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/4/7
+ */
+public class Solution {
+
+    public int missingNumber(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (nums[m] == m) {
+                i = m + 1;
+            } else {
+                j = m - 1;
+            }
+        }
+        return i;
+    }
+}
