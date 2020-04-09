dbd4165cd58c381a3b72ea7beb8ab4e619a95e04
heshan <5373827@qq.com>
2020-04-09 21:18:57 +0800

    l17_01

---
+++ src/main/java/m17_01/Solution.java
@@ -0,0 +1,24 @@
+package m17_01;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/4/9
+ */
+public class Solution {
+
+    public int add(int a, int b) {
+        int res = a ^ b;
+        int d = (a & b) << 1;
+
+        while (d != 0) {
+            int tmp = (d & res) << 1;
+            res = res ^ d;
+
+            d = tmp;
+        }
+
+        return res;
+    }
+}
