904b8d7cc2620351245f9685932020ee839fff35
heshan <5373827@qq.com>
2020-04-13 18:07:32 +0800

    m08_11

---
+++ src/main/java/m08_11/Solution.java
@@ -0,0 +1,32 @@
+package m08_11;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/4/13
+ */
+public class Solution {
+
+    public int waysToChange(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+
+        int[] coins = new int[]{1, 5, 10, 25};
+        for (int c : coins) {
+            for (int i = 0; i <= n; i++) {
+                if (i + c <= n) {
+                    dp[i + c] += dp[i];
+                    dp[i + c] %= 1000000007;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        int res = new Solution().waysToChange(10);
+        System.out.println(res);
+    }
+}
