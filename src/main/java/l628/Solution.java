fcf1103ed79e8ef929496fd362fde77bdb5cc790
scheshan <5373827@qq.com>
2020-04-18 08:10:14 +0800

    l628

--- src/main/java/l628/Solution.java
+++ src/main/java/l628/Solution.java
@@ -1,7 +1,5 @@
 package l628;
 
-import java.util.Arrays;
-
 /**
  * Solution
  *
@@ -11,10 +9,31 @@ import java.util.Arrays;
 public class Solution {
 
     public int maximumProduct(int[] nums) {
-        Arrays.sort(nums);
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
 
-        int n1 = nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3];
-        int n2 = nums[nums.length - 1] * nums[0] * nums[1];
+        for (int n : nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+
+        int n1 = max1 * max2 * max3;
+        int n2 = max1 * min1 * min2;
         return Math.max(n1, n2);
     }
 }
