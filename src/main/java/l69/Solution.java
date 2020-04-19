ca8b8209c5f10cf7a1cb7277d81ac20d1e39849c
scheshan <5373827@qq.com>
2020-04-19 13:34:58 +0800

    l69

---
+++ src/main/java/l69/Solution.java
@@ -0,0 +1,27 @@
+package l69;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/4/19
+ */
+public class Solution {
+
+    public int mySqrt(int x) {
+        int left = 1;
+        int right = x;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid == x / mid) {
+                return mid;
+            } else if (mid > x / mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left - 1;
+    }
+}
