bbb2415861dc5c2eb2180dbde491b54ba951a965
scheshan <5373827@qq.com>
2020-04-19 13:37:58 +0800

    l278

---
+++ src/main/java/l278/Solution.java
@@ -0,0 +1,27 @@
+package l278;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/4/19
+ */
+public class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
---
+++ src/main/java/l278/VersionControl.java
@@ -0,0 +1,14 @@
+package l278;
+
+/**
+ * VersionControl
+ *
+ * @author heshan
+ * @date 2020/4/19
+ */
+public class VersionControl {
+
+    protected boolean isBadVersion(int version) {
+        return false;
+    }
+}
