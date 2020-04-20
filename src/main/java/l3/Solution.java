57c5e54b9e861d12defe7bcec5814a4517a8e718
heshan <5373827@qq.com>
2020-04-20 15:11:16 +0800

    l3

--- src/main/java/l3/Solution.java
+++ src/main/java/l3/Solution.java
@@ -1,9 +1,6 @@
 package l3;
 
-import javax.swing.*;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -21,26 +18,20 @@ public class Solution {
         int left = 0;
         int right = 0;
 
-        while (left < s.length() && right < s.length()) {
-            if (set.contains(s.charAt(right))) {
-                while (s.charAt(left) != s.charAt(right)) {
-                    set.remove(s.charAt(left));
-                    left++;
-                }
-                set.remove(s.charAt(left));
-                left++;
+        while (right < s.length()) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right++));
+                res = Math.max(res, set.size());
+            } else {
+                set.remove(s.charAt(left++));
             }
-
-            set.add(s.charAt(right));
-
-            res = Math.max(res, right - left + 1);
-            right++;
         }
 
         return res;
     }
 
     public static void main(String[] args) {
-        new Solution().lengthOfLongestSubstring("abcabcbb");
+        int res = new Solution().lengthOfLongestSubstring("pwwkew");
+        System.out.println(res);
     }
 }
