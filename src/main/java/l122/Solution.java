2c63ccbc52a13a3fd56247c86b55e526a41a779d
scheshan <5373827@qq.com>
2020-04-21 20:08:54 +0800

    l122

--- src/main/java/l122/Solution.java
+++ src/main/java/l122/Solution.java
@@ -9,43 +9,26 @@ package l122;
 public class Solution {
 
     public int maxProfit(int[] prices) {
-        int res = 0;
+        int i1 = 0, j1 = 0, i2 = 0, j2 = 0;
 
-        if (prices.length < 2) {
-            return res;
-        }
-
-        boolean up = true;
-        int min = prices[0];
-        int max = prices[0];
-
-        for (int i = 1; i < prices.length; i++) {
-            if (!up) {
-                if (prices[i] <= prices[i - 1]) {
-                    min = prices[i];
-                } else {
-                    up = true;
-                    max = prices[i];
-                }
+        for (int i = 0; i < prices.length; i++) {
+            if (i == 0) {
+                i2 = 0;
+                j2 = -prices[i];
             } else {
-                if (prices[i] >= prices[i - 1]) {
-                    max = prices[i];
-                } else {
-                    up = false;
-                    res += Math.max(0, max - min);
-                    min = prices[i];
-                }
+                i2 = Math.max(i1, j1 + prices[i]);
+                j2 = Math.max(j1, i1 - prices[i]);
             }
+
+            i1 = i2;
+            j1 = j2;
         }
 
-        if (up) {
-            res += Math.max(0, max - min);
-        }
-
-        return res;
+        return i1;
     }
 
     public static void main(String[] args) {
-        new Solution().maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+        int res = new Solution().maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+        System.out.println(res);
     }
 }
