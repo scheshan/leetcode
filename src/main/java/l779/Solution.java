b2f2e85ed281d38edaafd777001e319c41726419
scheshan <5373827@qq.com>
2020-04-25 14:28:18 +0800

    l779

---
+++ src/main/java/l779/Solution.java
@@ -0,0 +1,23 @@
+package l779;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/4/25
+ */
+public class Solution {
+
+    public int kthGrammar(int N, int K) {
+        if (N == 1) {
+            return 0;
+        }
+
+        int n = kthGrammar(N - 1, (K + 1) / 2);
+        if ((K & 1) == 1) {
+            return 1 - n;
+        } else {
+            return n;
+        }
+    }
+}
