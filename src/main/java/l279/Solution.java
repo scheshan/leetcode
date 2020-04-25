518bfff46239b803852860eefda7370c2159314d
scheshan <5373827@qq.com>
2020-04-25 12:04:12 +0800

    l279

--- src/main/java/l279/Solution.java
+++ src/main/java/l279/Solution.java
@@ -15,9 +15,9 @@ public class Solution {
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
 
-        for (int i = 0; i <= n; i++) {
-            for (int j = 1; j * j + i <= n; j++) {
-                dp[i + j * j] = Math.min(dp[i + j * j], dp[i] + 1);
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
 
