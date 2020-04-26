b579e7da1dc562ca32fc32515dd7cac04b021983
scheshan <5373827@qq.com>
2020-04-26 21:08:07 +0800

    l422

---
+++ src/main/java/l422/Solution.java
@@ -0,0 +1,26 @@
+package l422;
+
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2020/4/26
+ */
+public class Solution {
+
+    public boolean validWordSquare(List<String> words) {
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.get(i).length(); j++) {
+                if (j >= words.size() || i >= words.get(j).length()) {
+                    return false;
+                }
+                if (words.get(i).charAt(j) != words.get(j).charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
