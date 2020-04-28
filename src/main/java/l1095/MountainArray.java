85cca91859dbb589e0a556d61934b657e6155784
scheshan <5373827@qq.com>
2020-04-29 07:17:43 +0800

    l1095

---
+++ src/main/java/l1095/MountainArray.java
@@ -0,0 +1,14 @@
+package l1095;
+
+/**
+ * MountainArray
+ *
+ * @author heshan
+ * @date 2020/4/29
+ */
+public interface MountainArray {
+
+    public int get(int index);
+
+    public int length();
+}
