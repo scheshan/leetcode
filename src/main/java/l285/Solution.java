78f9b17d439c447f19760f33523c8a3427f34294
heshan <5373827@qq.com>
2020-05-14 22:17:36 +0800

    l285

--- src/main/java/l285/Solution.java
+++ src/main/java/l285/Solution.java
@@ -10,27 +10,18 @@ import shared.TreeNode;
  */
 public class Solution {
 
-    TreeNode res;
-
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        travel(root, p);
-        return res;
-    }
+        TreeNode res = null;
 
-    private void travel(TreeNode node, TreeNode p) {
-        if (node == null) {
-            return;
-        }
-
-        if (node.val > p.val) {
-            res = node;
-            travel(node.left, p);
-        } else if (node.val < p.val) {
-            travel(node.right, p);
-        } else {
-            if (node.right != null) {
-                travel(node.right, p);
+        while (root != null) {
+            if (root.val > p.val) {
+                res = root;
+                root = root.left;
+            } else {
+                root = root.right;
             }
         }
+
+        return res;
     }
 }
