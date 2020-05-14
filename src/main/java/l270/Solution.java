92385011cc721b4e8047987306fcf6aa54d3ff89
heshan <5373827@qq.com>
2020-05-14 22:14:20 +0800

    l270

--- src/main/java/l270/Solution.java
+++ src/main/java/l270/Solution.java
@@ -9,26 +9,21 @@ import shared.TreeNode;
  * @date 2020/5/14
  */
 public class Solution {
-
-    Integer res = null;
-
     public int closestValue(TreeNode root, double target) {
-        travel(root, target);
+        int res = root.val;
+
+        while (root != null) {
+            if (Math.abs(root.val - target) < Math.abs(res - target)) {
+                res = root.val;
+            }
+
+            if (root.val < target) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+
         return res;
-    }
-
-    private void travel(TreeNode node, double target) {
-        if (node == null) {
-            return;
-        }
-
-        if (res == null || Math.abs(node.val - target) < Math.abs(res - target)) {
-            res = node.val;
-        }
-        if (node.val < target) {
-            travel(node.right, target);
-        } else {
-            travel(node.left, target);
-        }
     }
 }
