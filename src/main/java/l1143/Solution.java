d2af8fc2db4bede965c8c28288dbc2361526be51
heshan <5373827@qq.com>
2020-05-20 17:39:01 +0800

    l1143

--- src/main/java/l1143/Solution.java
+++ src/main/java/l1143/Solution.java
@@ -9,21 +9,24 @@ package l1143;
 public class Solution {
 
     public int longestCommonSubsequence(String text1, String text2) {
+        int[] pre = new int[text2.length() + 1];
+        int[] cur = new int[text2.length() + 1];
+
         int res = 0;
-
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-
-        for (int i = 0; i < text1.length(); i++) {
-            for (int j = 0; j < text2.length(); j++) {
-                int n = 0;
-                if (text1.charAt(i) == text2.charAt(j)) {
-                    n = dp[i][j] + 1;
-                } else {
-                    n = Math.max(dp[i][j + 1], dp[i + 1][j]);
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                cur[j] = 0;
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    cur[j] = pre[j - 1] + 1;
                 }
-                dp[i + 1][j + 1] = n;
-                res = Math.max(res, n);
+                cur[j] = Math.max(cur[j], pre[j]);
+                cur[j] = Math.max(cur[j], cur[j - 1]);
+
+                res = Math.max(res, cur[j]);
             }
+            int[] t = pre;
+            pre = cur;
+            cur = t;
         }
 
         return res;
