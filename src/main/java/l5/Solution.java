395314f4dde68bbfa652e01c587f721d9f8de45b
heshan <5373827@qq.com>
2020-05-21 08:53:02 +0800

    l5

--- src/main/java/l5/Solution.java
+++ src/main/java/l5/Solution.java
@@ -8,56 +8,40 @@ package l5;
  */
 public class Solution {
 
+    int left = 0;
+
+    int right = 0;
+
     public String longestPalindrome(String s) {
         if (s.length() == 0) {
             return s;
         }
 
-        Range res = new Range(0, 0);
         for (int i = 0; i < s.length(); i++) {
-            res = max(res, count(s, i, i));
+            count(s, i, i);
             if (i < s.length() - 1) {
-                res = max(res, count(s, i, i + 1));
+                count(s, i, i + 1);
             }
         }
 
-        return new String(s.toCharArray(), res.left, res.right - res.left + 1);
+        return new String(s.toCharArray(), left, right - left + 1);
     }
 
-    private Range count(String s, int left, int right) {
-        Range res = new Range(left, left);
-
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            res.left = left;
-            res.right = right;
-
-            left--;
-            right++;
+    private void count(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
         }
 
-        return res;
-    }
-
-    private Range max(Range x, Range y) {
-        if (y.right - y.left > x.right - x.left) {
-            return y;
-        }
-        return x;
-    }
-
-    private class Range {
-
-        int left;
-
-        int right;
-
-        public Range(int left, int right) {
-            this.left = left;
-            this.right = right;
+        l = l + 1;
+        r = r - 1;
+        if (r - l > right - left) {
+            right = r;
+            left = l;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().longestPalindrome("ccc"));
+        System.out.println(new Solution().longestPalindrome("a"));
     }
 }
