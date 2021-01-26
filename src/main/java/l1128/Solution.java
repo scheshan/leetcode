54bb649206dd64c369afb0d278c453441d7a69bf
heshan <5373827@qq.com>
2021-01-26 08:08:06 +0800

    l1128

--- src/main/java/l1128/Solution.java
+++ src/main/java/l1128/Solution.java
@@ -9,22 +9,18 @@ package l1128;
 public class Solution {
 
     public int numEquivDominoPairs(int[][] dominoes) {
-        int[][] map = new int[10][10];
+        int[] map = new int[100];
+
+        int res = 0;
 
         for (int[] domino : dominoes) {
             int min = Math.min(domino[0], domino[1]);
             int max = Math.max(domino[0], domino[1]);
 
-            map[min][max]++;
-        }
+            int ind = min * 10 + max;
 
-        int res = 0;
-        for (int i = 1; i <= 9; i++) {
-            for (int j = 1; j <= 9; j++) {
-                if (map[i][j] > 1) {
-                    res += map[i][j] * (map[i][j] - 1) / 2;
-                }
-            }
+            res += map[ind];
+            map[ind]++;
         }
 
         return res;
