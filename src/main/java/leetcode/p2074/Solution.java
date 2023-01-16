36989cf03941bf27e3a217f476f6c8cf6a583b20
heshan <5373827@qq.com>
2023-01-16 21:19:19 +0800

    p2074

---
+++ src/main/java/leetcode/p2074/Solution.java
@@ -0,0 +1,61 @@
+package leetcode.p2074;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/16
+ */
+public class Solution {
+
+    public ListNode reverseEvenLengthGroups(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        int cnt = 0;
+        while (head != null) {
+            cnt++;
+            int i = 0;
+            ListNode pre = null;
+            ListNode h = head;
+            for (; i < cnt && h != null; i++) {
+                pre = h;
+                h = h.next;
+            }
+            pre.next = null;
+
+            if ((i & 1) == 1) {
+                tail.next = head;
+                tail = pre;
+            } else {
+                tail.next = reverse(head);
+                tail = head;
+            }
+
+            head = h;
+        }
+
+        return dummy.next;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode res = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = res;
+            res = head;
+            head = next;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+
+        new Solution().reverseEvenLengthGroups(head);
+    }
+}
