01b6b1431f4d70500ce2a0ba08f3bac728f52fad
heshan <heshan@baicizhan.com>
2023-01-16 13:28:43 +0800

    p160

---
+++ src/main/java/leetcode/p160/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.p160;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/16
+ */
+public class Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+
+        while (l1 != l2) {
+            if (l1 == null) {
+                l1 = headB;
+            } else {
+                l1 = l1.next;
+            }
+            if (l2 == null) {
+                l2 = headA;
+            } else {
+                l2 = l2.next;
+            }
+        }
+
+        return l1;
+    }
+}
