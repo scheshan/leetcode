753c1db3931dc7c91df4c5ee0fff583ce4a2c07f
heshan <heshan@baicizhan.com>
2023-01-16 18:28:29 +0800

    interview p02_05

---
+++ src/main/java/interview/p02_05/Solution.java
@@ -0,0 +1,42 @@
+package interview.p02_05;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/16
+ */
+public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        boolean flag = false;
+
+        while (l1 != null || l2 != null || flag) {
+            int n = 0;
+            if (flag) {
+                n++;
+                flag = false;
+            }
+            if (l1 != null) {
+                n += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                n += l2.val;
+                l2 = l2.next;
+            }
+            if (n >= 10) {
+                n -= 10;
+                flag = true;
+            }
+            tail.next = new ListNode(n);
+            tail = tail.next;
+        }
+
+        return dummy.next;
+    }
+}
