789b6f4e18297db4c2184932102905b6112354cb
heshan <heshan@baicizhan.com>
2023-01-16 13:17:42 +0800

    offer2 p022

---
+++ src/main/java/offer2/p022/Solution.java
@@ -0,0 +1,47 @@
+package offer2.p022;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/16
+ */
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (slow != null && fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+            }
+            slow = slow.next;
+
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null) {
+            return null;
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+
+        new Solution().detectCycle(head);
+    }
+}
