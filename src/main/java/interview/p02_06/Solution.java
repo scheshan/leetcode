8ab2a3cf8bd7af2f67f629ea85df19d6ed5ddd24
heshan <5373827@qq.com>
2023-01-16 19:41:44 +0800

    interview p02_06

---
+++ src/main/java/interview/p02_06/Solution.java
@@ -0,0 +1,46 @@
+package interview.p02_06;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/16
+ */
+public class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) {
+            return true;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        ListNode right = null;
+        ListNode node = slow.next;
+        slow.next = null;
+        while (node != null) {
+            ListNode next = node.next;
+            node.next = right;
+            right = node;
+            node = next;
+        }
+
+        while (head != null && right != null) {
+            if (head.val != right.val) {
+                return false;
+            }
+            head = head.next;
+            right = right.next;
+        }
+
+        return head == null || head.next == null;
+    }
+}
