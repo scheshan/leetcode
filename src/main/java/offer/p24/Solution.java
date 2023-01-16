c8f2fe7d0ca96a41fea829260a280cb5024981fe
heshan <heshan@baicizhan.com>
2023-01-16 11:18:00 +0800

    offer p24

---
+++ src/main/java/offer/p24/Solution.java
@@ -0,0 +1,24 @@
+package offer.p24;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/16
+ */
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode res = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = res;
+            res = head;
+            head = next;
+        }
+
+        return res;
+    }
+}
