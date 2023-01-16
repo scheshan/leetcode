a96b24e32766f375983813c1134de5434f0a1cce
heshan <heshan@baicizhan.com>
2023-01-16 13:27:31 +0800

    offer2 p023

---
+++ src/main/java/offer2/p023/Solution.java
@@ -0,0 +1,43 @@
+package offer2.p023;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/16
+ */
+public class Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode l1 = headA;
+        ListNode l2 = headB;
+
+        while (l1 != l2) {
+            if (l1 == null) {
+                l1 = headB;
+            } else {
+                l1 = l1.next;
+            }
+            if (l2 == null) {
+                l2 = headA;
+            } else {
+                l2 = l2.next;
+            }
+        }
+
+        return l1;
+    }
+
+    public static void main(String[] args) {
+        ListNode headA = new ListNode(1);
+        headA.next = new ListNode(2);
+        headA.next.next = new ListNode(3);
+
+        ListNode headB = new ListNode(4);
+        headB.next = headA;
+
+        new Solution().getIntersectionNode(headA, headB);
+    }
+}
