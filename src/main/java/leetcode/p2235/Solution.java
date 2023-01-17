b51b6991e06135085eaf3f53da60a9e810e2f7ba
heshan <heshan@baicizhan.com>
2023-01-17 18:26:35 +0800

    p2235

---
+++ src/main/java/leetcode/p2235/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.p2235;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/17
+ */
+public class Solution {
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
