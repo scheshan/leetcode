bce5d5b0e24318017d5717f8dfdcad2bb5005eb1
heshan <heshan@baicizhan.com>
2023-01-17 11:22:42 +0800

    p116

---
+++ src/main/java/leetcode/p116/Solution.java
@@ -0,0 +1,75 @@
+package leetcode.p116;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/17
+ */
+public class Solution {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node pre = null;
+            for (int i = 0; i < size; i++) {
+                Node node = queue.remove();
+                if (pre != null) {
+                    pre.next = node;
+                }
+                pre = node;
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public void test() {
+        Node root = new Node(1);
+        root.left = new Node(2, new Node(4), new Node(5), null);
+        root.right = new Node(3, new Node(6), new Node(7), null);
+
+        this.connect(root);
+    }
+
+    public static void main(String[] args) {
+        new Solution().test();
+    }
+}
