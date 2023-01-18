942bc3f443db1c719a3886afea3ad0486d19aaee
heshan <heshan@baicizhan.com>
2023-01-18 16:16:23 +0800

    p104

---
+++ src/main/java/leetcode/p104/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.p104;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/18
+ */
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int res = Math.max(maxDepth(root.left), maxDepth(root.right));
+        return res + 1;
+    }
+}
