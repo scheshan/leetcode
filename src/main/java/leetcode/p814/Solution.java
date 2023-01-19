d4871694498922f0d3a017b6b4ac82faa378a259
heshan <heshan@baicizhan.com>
2023-01-19 14:06:03 +0800

    p814

---
+++ src/main/java/leetcode/p814/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p814;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/19
+ */
+public class Solution {
+
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (root.left == null && root.right == null && root.val == 0) {
+            return null;
+        }
+        return root;
+    }
+}
