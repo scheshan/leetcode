2fe1b94ad980155419c8fa734f0caf84a7f805cb
heshan <5373827@qq.com>
2023-01-20 07:57:27 +0800

    offer p68_02

---
+++ src/main/java/offer/p68_02/Solution.java
@@ -0,0 +1,32 @@
+package offer.p68_02;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/20
+ */
+public class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        } else if (left == null && right == null) {
+            return null;
+        }
+        return left == null ? right : left;
+    }
+}
