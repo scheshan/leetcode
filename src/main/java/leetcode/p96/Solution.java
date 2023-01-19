15449af570b829bc5dba1ad6761cdb61fcd4ff4b
heshan <5373827@qq.com>
2023-01-19 21:44:10 +0800

    p96

---
+++ src/main/java/leetcode/p96/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.p96;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/19
+ */
+public class Solution {
+
+    public int numTrees(int n) {
+        int[] state = new int[n + 1];
+        state[0] = 1;
+        state[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int cnt = 0;
+            for (int cur = 1; cur <= i; cur++) {
+                int left = cur - 1;
+                int right = i - cur;
+                cnt += state[left] * state[right];
+            }
+            state[i] = cnt;
+        }
+
+        return state[n];
+    }
+
+    public static void main(String[] args) {
+        new Solution().numTrees(2);
+    }
+}
