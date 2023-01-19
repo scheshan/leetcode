fc1978cb5c6e26449b3a9b5604afd907a2e4dbdf
heshan <heshan@baicizhan.com>
2023-01-19 12:35:29 +0800

    offer2 p046

---
+++ src/main/java/offer2/p046/Solution.java
@@ -0,0 +1,34 @@
+package offer2.p046;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/19
+ */
+public class Solution {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, 0, res);
+        return res;
+    }
+
+    private void dfs(TreeNode node, int level, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+
+        if (res.size() == level) {
+            res.add(node.val);
+        }
+
+        dfs(node.right, level + 1, res);
+        dfs(node.left, level + 1, res);
+    }
+}
