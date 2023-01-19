1226c7781370d94d1a2d69b8dd6ffc389c51ab68
heshan <5373827@qq.com>
2023-01-19 20:25:30 +0800

    offer p48

---
+++ src/main/java/offer/p48/Solution.java
@@ -0,0 +1,44 @@
+package offer.p48;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/19
+ */
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        int res = 0;
+
+        int l = 0;
+        int r = 0;
+        while (r < s.length()) {
+            map.compute(s.charAt(r++), (k, v) -> {
+                if (v == null) {
+                    v = 0;
+                }
+                return ++v;
+            });
+
+            while (map.size() < r - l) {
+                char ch = s.charAt(l++);
+                int count = map.get(ch);
+                if (count == 1) {
+                    map.remove(ch);
+                } else {
+                    map.put(ch, count - 1);
+                }
+            }
+
+            res = Math.max(res, r - l);
+        }
+
+        return res;
+    }
+}
