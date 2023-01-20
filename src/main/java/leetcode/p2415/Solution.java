b81a67d6a85ce407482f61a105b3588f6108c355
heshan <5373827@qq.com>
2023-01-20 20:52:27 +0800

    p2415

---
+++ src/main/java/leetcode/p2415/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.p2415;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/20
+ */
+public class Solution {
+
+    public TreeNode reverseOddLevels(TreeNode root) {
+        dfs(root, root, 0);
+        return root;
+    }
+
+    private void dfs(TreeNode left, TreeNode right, int level) {
+        if (left == null) {
+            return;
+        }
+
+        if ((level & 1) == 1) {
+            int tmp = left.val;
+            left.val = right.val;
+            right.val = tmp;
+        }
+
+        dfs(left.left, right.right, level + 1);
+        if (left != right) {
+            dfs(left.right, right.left, level + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        new Solution().reverseOddLevels(root);
+    }
+}
