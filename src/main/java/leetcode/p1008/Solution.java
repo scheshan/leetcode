f3d1df2638e564722e3be0e3b67c63e03deccb23
heshan <5373827@qq.com>
2023-01-20 17:00:59 +0800

    p1008

---
+++ src/main/java/leetcode/p1008/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.p1008;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/20
+ */
+public class Solution {
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return build(preorder, 0, preorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int l, int r) {
+        if (l > r) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(preorder[l]);
+        int mid = l + 1;
+        while (mid <= r && preorder[mid] < node.val) {
+            mid++;
+        }
+        node.left = build(preorder, l + 1, mid - 1);
+        node.right = build(preorder, mid, r);
+        return node;
+    }
+
+    public static void main(String[] args) {
+        new Solution().bstFromPreorder(new int[]{4, 2});
+    }
+}
