79a22781e61bdadf8afb254ff9a77b6a0839fcb4
heshan <5373827@qq.com>
2023-01-22 09:29:24 +0800

    offer2 p060

---
+++ src/main/java/offer2/p060/Solution.java
@@ -0,0 +1,47 @@
+package offer2.p060;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/22
+ */
+public class Solution {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.compute(num, (key, v) -> {
+                if (v == null) {
+                    v = 0;
+                }
+                return ++v;
+            });
+        }
+
+        Queue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((o1, o2) -> {
+            return o1.getValue() - o2.getValue();
+        });
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            if (queue.size() < k) {
+                queue.add(entry);
+            } else {
+                if (queue.peek().getValue() < entry.getValue()) {
+                    queue.remove();
+                    queue.add(entry);
+                }
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = queue.remove().getKey();
+        }
+        return res;
+    }
+}
