ac0a9f0db421203414bb82c122a9728742e05c37
heshan <5373827@qq.com>
2023-01-23 07:45:40 +0800

    p589

---
+++ src/main/java/leetcode/p589/Solution.java
@@ -0,0 +1,46 @@
+package leetcode.p589;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/23
+ */
+public class Solution {
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(Node node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        res.add(node.val);
+        for (Node child : node.children) {
+            dfs(child, res);
+        }
+    }
+}
