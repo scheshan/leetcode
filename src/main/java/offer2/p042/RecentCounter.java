0e13a519d83da34205605ccdbedb7261cb49ad07
heshan <5373827@qq.com>
2023-01-23 13:57:29 +0800

    offer2 p042

---
+++ src/main/java/offer2/p042/RecentCounter.java
@@ -0,0 +1,30 @@
+package offer2.p042;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/23
+ */
+public class RecentCounter {
+
+    Queue<Integer> queue;
+
+    int total;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+        total = 0;
+    }
+
+    public int ping(int t) {
+        while (!queue.isEmpty() && queue.peek() < t - 3000) {
+            total -= queue.remove();
+        }
+        queue.add(t);
+        return queue.size();
+    }
+}
