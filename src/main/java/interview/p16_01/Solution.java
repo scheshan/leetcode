93cd2cf00d4774aa889493bcef071ea3bc83e357
heshan <5373827@qq.com>
2023-01-23 11:30:02 +0800

    interview p16_01

---
+++ src/main/java/interview/p16_01/Solution.java
@@ -0,0 +1,14 @@
+package interview.p16_01;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/23
+ */
+public class Solution {
+
+    public int[] swapNumbers(int[] numbers) {
+        return new int[]{numbers[1], numbers[0]};
+    }
+}
