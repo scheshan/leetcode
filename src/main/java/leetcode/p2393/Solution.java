8adc93dbc2fa2bb48851c318245a03642f34d57f
heshan <5373827@qq.com>
2023-01-26 12:20:56 +0800

    p2393

---
+++ src/main/java/leetcode/p2393/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.p2393;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/26
+ */
+public class Solution {
+
+    public long countSubarrays(int[] nums) {
+        long res = 0;
+
+        int pre = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0 || nums[i] > nums[i - 1]) {
+                continue;
+            }
+
+            long len = i - pre;
+            res += ((1 + len) * len) >> 1;
+            pre = i;
+        }
+        long len = nums.length - pre;
+        res += (1 + len) * (len >> 1);
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new Solution().countSubarrays(new int[]{1, 3, 5, 4, 4, 6});
+    }
+}
