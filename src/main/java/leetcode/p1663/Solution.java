cbff58ab417b19d0657db2237ae56f05f63adca0
heshan <5373827@qq.com>
2023-01-26 13:31:25 +0800

    p1663

---
+++ src/main/java/leetcode/p1663/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.p1663;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/26
+ */
+public class Solution {
+
+    public String getSmallestString(int n, int k) {
+        char[] arr = new char[n];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int ch;
+            if (k > 26 + i) {
+                ch = 26;
+            } else {
+                ch = k - i;
+            }
+            k -= ch;
+            arr[i] = (char) ('a' + ch - 1);
+        }
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        new Solution().getSmallestString(5, 73);
+    }
+}
