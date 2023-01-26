dd4139e1bab8017a0138b6e604e584bc2f60273e
heshan <5373827@qq.com>
2023-01-26 11:47:47 +0800

    p198

---
+++ src/main/java/leetcode/p198/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.p198;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/26
+ */
+public class Solution {
+
+    public int rob(int[] nums) {
+        int[] state = new int[2];
+        for (int num : nums) {
+            int s0 = state[1] + num;
+            int s1 = state[0];
+
+            state[0] = Math.max(state[0], s0);
+            state[1] = Math.max(state[1], s1);
+        }
+
+        return Math.max(state[0], state[1]);
+    }
+}
