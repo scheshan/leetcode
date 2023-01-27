e55e7c3074437f3071715245a91996901f268395
heshan <5373827@qq.com>
2023-01-27 19:58:38 +0800

    p209

---
+++ src/main/java/leetcode/p209/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.p209;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    public int minSubArrayLen(int target, int[] nums) {
+        int res = nums.length + 1;
+
+        int l = 0;
+        int r = 0;
+        int cur = 0;
+        while (r < nums.length) {
+            cur += nums[r];
+
+            while (cur >= target) {
+                res = Math.min(res, r - l + 1);
+                cur -= nums[l++];
+            }
+            r++;
+        }
+
+        if (res == nums.length + 1) {
+            return 0;
+        }
+        return res;
+    }
+}
