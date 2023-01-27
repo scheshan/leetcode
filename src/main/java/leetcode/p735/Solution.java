479e18154034160f342ed9e236bcdeb83ea2e1e5
heshan <5373827@qq.com>
2023-01-27 20:14:40 +0800

    p735

---
+++ src/main/java/leetcode/p735/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.p735;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    public int[] asteroidCollision(int[] asteroids) {
+        List<Integer> list = new ArrayList<>();
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int num : asteroids) {
+            if (num > 0) {
+                deque.addLast(num);
+            } else {
+                while (!deque.isEmpty() && deque.peekLast() <= -num) {
+                    if (deque.removeLast() == -num) {
+                        num = 0;
+                    }
+                }
+                if (num != 0 && deque.isEmpty()) {
+                    list.add(num);
+                }
+            }
+        }
+        list.addAll(deque);
+
+        int[] res = new int[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
