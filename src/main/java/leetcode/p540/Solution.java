c44424e86c7fbdae861d361e78b7ddebba2cb98e
heshan <5373827@qq.com>
2023-01-28 07:35:42 +0800

    p540

---
+++ src/main/java/leetcode/p540/Solution.java
@@ -0,0 +1,46 @@
+package leetcode.p540;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/28
+ */
+public class Solution {
+
+    public int singleNonDuplicate(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int l = 0;
+        int r = nums.length;
+
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+
+            if (mid == 0 && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            } else if (mid == nums.length - 1 && nums[mid] != nums[mid - 1]) {
+                return nums[mid];
+            } else if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+
+            if ((mid & 1) == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            } else {
+                if (nums[mid] == nums[mid - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+        }
+        return -1;
+    }
+}
