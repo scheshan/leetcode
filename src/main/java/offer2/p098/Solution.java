f8055115c60e34114994ce34b3a4522dff4888d7
heshan <5373827@qq.com>
2023-01-27 11:42:01 +0800

    p098

---
+++ src/main/java/offer2/p098/Solution.java
@@ -0,0 +1,24 @@
+package offer2.p098;
+
+import java.util.Arrays;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int row = 1; row < m; row++) {
+            for (int col = 1; col < n; col++) {
+                dp[col] += dp[col - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+}
