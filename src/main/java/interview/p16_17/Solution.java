53787a6f400db068de660c03ffc6089c01d39a31
heshan <5373827@qq.com>
2023-01-27 10:40:30 +0800

    p16_17

---
+++ src/main/java/interview/p16_17/Solution.java
@@ -0,0 +1,24 @@
+package interview.p16_17;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    public int maxSubArray(int[] nums) {
+        int res = Integer.MIN_VALUE;
+
+        int cur = 0;
+        for (int num : nums) {
+            cur += num;
+            res = Math.max(res, cur);
+            if (cur < 0) {
+                cur = 0;
+            }
+        }
+        return res;
+    }
+}
