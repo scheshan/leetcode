1c51b1870b685331aad4f8ccaad3fdebd486f761
heshan <5373827@qq.com>
2023-01-27 16:53:34 +0800

    p14_01

---
+++ src/main/java/offer/p14_01/Solution.java
@@ -0,0 +1,40 @@
+package offer.p14_01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public int cuttingRope(int n) {
+        int res = 0;
+        for (int i = 1; i <= n >> 1; i++) {
+            res = Math.max(res, count(i) * count(n - i));
+        }
+        return res;
+    }
+
+    private int count(int n) {
+        if (n <= 4) {
+            return n;
+        }
+
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+
+        int res = n;
+        for (int i = 1; i <= n >> 1; i++) {
+            res = Math.max(res, count(i) * count(n - i));
+        }
+        map.put(n, res);
+        return res;
+    }
+}
