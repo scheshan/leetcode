aaa560dbffea706ec0fd44e5a98ce3d4474b9dc3
heshan <5373827@qq.com>
2023-01-27 13:01:33 +0800

    p069

---
+++ src/main/java/offer2/p069/Solution.java
@@ -0,0 +1,35 @@
+package offer2.p069;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int l = 0;
+        int r = arr.length;
+
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            if (mid == 0 && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid == arr.length - 1 && arr[mid] > arr[mid - 1]) {
+                return mid;
+            }
+            if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+
+            if (arr[mid] > arr[mid - 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return -1;
+    }
+}
