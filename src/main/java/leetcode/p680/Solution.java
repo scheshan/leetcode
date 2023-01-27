c1c727885fcdd46f849373d08b63fa28b33f9463
heshan <5373827@qq.com>
2023-01-27 19:41:49 +0800

    p680

---
+++ src/main/java/leetcode/p680/Solution.java
@@ -0,0 +1,37 @@
+package leetcode.p680;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } else {
+                return valid(s, l + 1, r) || valid(s, l, r - 1);
+            }
+        }
+        return true;
+    }
+
+    private boolean valid(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) == s.charAt(r)) {
+                l++;
+                r--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
