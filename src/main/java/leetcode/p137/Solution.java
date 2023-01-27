0136aff4264677edd24ea6a59e2287d67f56352c
heshan <5373827@qq.com>
2023-01-27 11:34:41 +0800

    p137

---
+++ src/main/java/leetcode/p137/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.p137;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class Solution {
+
+    public int singleNumber(int[] nums) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int cnt = 0;
+            for (int num : nums) {
+                if ((num & (1 << i)) != 0) {
+                    cnt++;
+                }
+            }
+            if (cnt % 3 != 0) {
+                res |= 1 << i;
+            }
+        }
+
+        return res;
+    }
+}
