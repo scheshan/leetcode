376070196c223964da02bcc631c804da48304fb5
heshan <5373827@qq.com>
2023-01-27 12:11:43 +0800

    p346

---
+++ src/main/java/leetcode/p346/MovingAverage.java
@@ -0,0 +1,40 @@
+package leetcode.p346;
+
+/**
+ * MovingAverage
+ *
+ * @author heshan
+ * @date 2023/1/27
+ */
+public class MovingAverage {
+
+    private int maxSize;
+    private int size;
+    private int[] queue;
+    private int start;
+    private int end;
+    private int sum;
+
+    public MovingAverage(int size) {
+        queue = new int[size];
+        this.maxSize = size;
+    }
+
+    public double next(int val) {
+        if (size == maxSize) {
+            sum -= queue[start];
+            start++;
+            if (start == queue.length) {
+                start = 0;
+            }
+            size--;
+        }
+        queue[end++] = val;
+        sum += val;
+        if (end == queue.length) {
+            end = 0;
+        }
+        size++;
+        return sum * 1.0 / size;
+    }
+}
