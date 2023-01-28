a82c65a225a15bf76c3a8f3c42d78418b5179a7e
heshan <5373827@qq.com>
2023-01-28 08:01:32 +0800

    p528

---
+++ src/main/java/leetcode/p528/Solution.java
@@ -0,0 +1,47 @@
+package leetcode.p528;
+
+import java.util.Random;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/28
+ */
+public class Solution {
+
+    private int[] w;
+
+    private int max;
+
+    private Random rnd;
+
+    public Solution(int[] w) {
+        for (int i = 1; i < w.length; i++) {
+            w[i] += w[i - 1];
+        }
+        this.w = w;
+        max = w[w.length - 1];
+        rnd = new Random();
+    }
+
+    public int pickIndex() {
+        int num = rnd.nextInt(max);
+
+        int l = 0;
+        int r = w.length;
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+
+            if (num >= w[mid]) {
+                l = mid + 1;
+            } else {
+                if (mid == 0 || num >= w[mid - 1]) {
+                    return mid;
+                }
+                r = mid;
+            }
+        }
+        return 0;
+    }
+}
