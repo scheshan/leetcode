9ac5607d0ed43b99b36b77c51b2e65ca36668432
heshan <5373827@qq.com>
2023-01-28 08:35:33 +0800

    p560

---
+++ src/main/java/leetcode/p560/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p560;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/28
+ */
+public class Solution {
+
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int res = 0;
+        int cur = 0;
+        for (int num : nums) {
+            cur += num;
+            if (cur == k) {
+                res++;
+            }
+            if (map.containsKey(cur - k)) {
+                res += map.get(cur - k);
+            }
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
+        }
+        return res;
+    }
+}
