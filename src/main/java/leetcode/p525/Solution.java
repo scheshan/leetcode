4ad07b3920780dea8b37031b8645b4db0121c13c
heshan <5373827@qq.com>
2023-01-28 08:16:37 +0800

    p525

---
+++ src/main/java/leetcode/p525/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.p525;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/28
+ */
+public class Solution {
+
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int res = 0;
+        int cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            cnt += nums[i] == 1 ? 1 : -1;
+            if (cnt == 0) {
+                res = Math.max(res, i + 1);
+            }
+
+            if (map.containsKey(cnt)) {
+                res = Math.max(res, i - map.get(cnt));
+            } else {
+                map.put(cnt, i);
+            }
+        }
+        return res;
+    }
+}
