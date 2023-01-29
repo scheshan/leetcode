a1c6f0adb6005d7e6f60425fccd97f15bf4b7e11
heshan <5373827@qq.com>
2023-01-29 11:53:37 +0800

    p946

---
+++ src/main/java/leetcode/p946/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p946;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/29
+ */
+public class Solution {
+
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        int l = 0;
+        int r = 0;
+        while (l < pushed.length && r < popped.length) {
+            if (deque.isEmpty() || deque.peekLast() != popped[r]) {
+                deque.addLast(pushed[l++]);
+            }
+
+            while (r < popped.length && !deque.isEmpty() && deque.peekLast() == popped[r]) {
+                deque.removeLast();
+                r++;
+            }
+        }
+        return l == pushed.length && deque.isEmpty();
+    }
+}
