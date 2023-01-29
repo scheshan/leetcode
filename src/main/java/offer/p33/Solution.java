a82fbd06254627d6f12d4021165f2c1cc4a09662
heshan <5373827@qq.com>
2023-01-29 20:34:02 +0800

    p33

---
+++ src/main/java/offer/p33/Solution.java
@@ -0,0 +1,34 @@
+package offer.p33;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/1/29
+ */
+public class Solution {
+
+    public boolean verifyPostorder(int[] postorder) {
+        return valid(postorder, 0, postorder.length - 1, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private boolean valid(int[] nums, int l, int r, int min, int max) {
+        if (l >= r) {
+            if (l > r) {
+                return true;
+            }
+            return nums[l] <= max && nums[l] >= min;
+        }
+
+        int mid = r;
+        while (mid >= l && nums[mid] >= nums[r]) {
+            mid--;
+        }
+
+        return valid(nums, l, mid, min, nums[r]) && valid(nums, mid + 1, r - 1, nums[r], max);
+    }
+
+    public static void main(String[] args) {
+        new Solution().verifyPostorder(new int[]{5, 4, 3, 2, 1});
+    }
+}
