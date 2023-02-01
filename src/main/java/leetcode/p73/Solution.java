0245fed0180ef06b09988bb2d3cfabda9473f71d
heshan <5373827@qq.com>
2023-02-01 20:52:18 +0800

    p73

---
+++ src/main/java/leetcode/p73/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.p73;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/2/1
+ */
+public class Solution {
+
+    public void setZeroes(int[][] matrix) {
+        boolean[] rows = new boolean[matrix.length];
+        boolean[] cols = new boolean[matrix[0].length];
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (matrix[row][col] == 0) {
+                    rows[row] = true;
+                    cols[col] = true;
+                }
+            }
+        }
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (rows[row] || cols[col]) {
+                    matrix[row][col] = 0;
+                }
+            }
+        }
+    }
+}
