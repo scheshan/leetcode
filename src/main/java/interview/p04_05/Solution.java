69b9c17eb8fcb4fb05e4ced8860d716f5f98646b
heshan <5373827@qq.com>
2023-02-01 19:36:42 +0800

    p04_05

---
+++ src/main/java/interview/p04_05/Solution.java
@@ -0,0 +1,26 @@
+package interview.p04_05;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/2/1
+ */
+public class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean dfs(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+        return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
+    }
+}
