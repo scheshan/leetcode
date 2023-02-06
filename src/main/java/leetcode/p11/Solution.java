53541ec9da009adfd2704e87273e931920e1113c
heshan <5373827@qq.com>
2023-02-06 21:33:28 +0800

    p11

---
+++ src/main/java/leetcode/p11/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p11;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/2/6
+ */
+public class Solution {
+
+    public int maxArea(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+
+        int res = 0;
+        while (l < r) {
+            res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return res;
+    }
+}
