f89b2d4a0ccd0ee3b7c418809e1f964fb92d85e2
heshan <5373827@qq.com>
2023-02-12 18:14:28 +0800

    p1858

---
+++ src/main/java/leetcode/p1858/Solution.java
@@ -0,0 +1,60 @@
+package leetcode.p1858;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/2/12
+ */
+public class Solution {
+
+    private class Node {
+        String value;
+        Node[] children;
+    }
+
+    private String res = "";
+
+    public String longestWord(String[] words) {
+        Node root = new Node();
+        root.value = "";
+        for (String word : words) {
+            insert(root, word);
+        }
+
+        dfs(root);
+        return res;
+    }
+
+    private void insert(Node root, String word) {
+        Node node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (node.children == null) {
+                node.children = new Node[26];
+            }
+            Node child = node.children[word.charAt(i) - 'a'];
+            if (child == null) {
+                child = new Node();
+                node.children[word.charAt(i) - 'a'] = child;
+            }
+            node = child;
+        }
+        node.value = word;
+    }
+
+    private void dfs(Node node) {
+        if (node == null || node.value == null) {
+            return;
+        }
+
+        if (node.value.length() > res.length()) {
+            res = node.value;
+        }
+
+        if (node.children != null) {
+            for (Node child : node.children) {
+                dfs(child);
+            }
+        }
+    }
+}
