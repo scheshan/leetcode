3173f3c1629366a6de26346473793f7df79a8301
heshan <5373827@qq.com>
2023-02-15 07:42:04 +0800

    p383

---
+++ src/main/java/leetcode/p383/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.p383;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/2/15
+ */
+public class Solution {
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            cnt[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            cnt[ransomNote.charAt(i) - 'a']--;
+            if (cnt[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
