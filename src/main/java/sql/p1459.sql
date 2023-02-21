c68f7058cb471f988787e234f2409fa077ed3ed0
heshan <heshan@baicizhan.com>
2023-02-21 11:25:35 +0800

    p1459

---
+++ src/main/java/sql/p1459.sql
@@ -0,0 +1,7 @@
+select
+A.id as p1,
+B.id as p2,
+abs(A.x_value - B.x_value) * abs(A.y_value - B.y_value) as area
+from Points A
+join Points B on A.id < B.id and A.x_value != B.x_value and A.y_value != B.y_value
+order by area desc, A.id asc, B.id asc
