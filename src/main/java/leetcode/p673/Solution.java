4e7687f0aedf5fe63b8f9eb0ac9b4d66daa8f964
heshan <5373827@qq.com>
2023-03-06 07:48:58 +0800

    p673

---
+++ src/main/java/leetcode/p673/Solution.java
@@ -0,0 +1,46 @@
+package leetcode.p673;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/3/6
+ */
+public class Solution {
+
+    public int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int[] cnt = new int[nums.length];
+        int max = 0;
+        int res = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            cnt[i]++;
+
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        cnt[i] = cnt[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        cnt[i] += cnt[j];
+                    }
+                }
+            }
+
+            if (dp[i] > max) {
+                max = dp[i];
+                res = cnt[i];
+            } else if (dp[i] == max) {
+                res += cnt[i];
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        new Solution().findNumberOfLIS(new int[]{1, 2, 4, 3, 5, 4, 7, 2});
+    }
+}
