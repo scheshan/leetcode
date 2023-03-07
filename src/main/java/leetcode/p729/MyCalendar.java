71b69567f0bca88b0df9ecf3330ff9a2ee57e2a0
heshan <5373827@qq.com>
2023-03-08 07:37:16 +0800

    p729

---
+++ src/main/java/leetcode/p729/MyCalendar.java
@@ -0,0 +1,38 @@
+package leetcode.p729;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * MyCalendar
+ *
+ * @author heshan
+ * @date 2023/3/8
+ */
+public class MyCalendar {
+
+    private TreeMap<Integer, Integer> map;
+
+    public MyCalendar() {
+        map = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        if (map.containsKey(start)) {
+            return false;
+        }
+
+        Map.Entry<Integer, Integer> e1 = map.lowerEntry(start);
+        if (e1 != null && e1.getValue() > start) {
+            return false;
+        }
+
+        Map.Entry<Integer, Integer> e2 = map.higherEntry(start);
+        if (e2 != null && e2.getKey() < end) {
+            return false;
+        }
+
+        map.put(start, end);
+        return true;
+    }
+}
