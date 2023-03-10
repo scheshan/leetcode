e5c6f7d717d51050d6332f749d312316bfbdca1a
heshan <heshan@baicizhan.com>
2023-03-10 15:46:07 +0800

    p139

---
+++ src/main/java/leetcode/p139/Solution.java
@@ -0,0 +1,34 @@
+package leetcode.p139;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/3/10
+ */
+public class Solution {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (String word : wordDict) {
+            set.add(word);
+        }
+
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.length()];
+    }
+}
