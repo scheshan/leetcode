ab3c3fb7322e5357e0f402baffcf7b6f71f6a4c5
heshan <5373827@qq.com>
2023-11-05 22:08:34 +0800

    p94

---
+++ src/main/java/leetcode/p94/Solution.java
@@ -0,0 +1,45 @@
+package leetcode.p94;
+
+import common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2023/11/5
+ */
+public class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.add(root);
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.removeLast();
+            if (node == null) {
+                res.add(deque.removeLast().val);
+            } else {
+                if (node.right != null) {
+                    deque.addLast(node.right);
+                }
+                deque.addLast(node);
+                deque.addLast(null);
+                if (node.left != null) {
+                    deque.addLast(node.left);
+                }
+            }
+        }
+
+        return res;
+    }
+}
