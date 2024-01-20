7c62d07741ecd6dba8ce1b9173bec4f03385db40
heshan <5373827@qq.com>
2024-01-21 06:32:45 +0800

    p9

---
+++ src/main/java/leetcode/p9/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.p9;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/21
+ */
+public class Solution {
+
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+
+        int l = 0, r = str.length() - 1;
+        while (l < r) {
+            if (str.charAt(l++) != str.charAt(r--)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
