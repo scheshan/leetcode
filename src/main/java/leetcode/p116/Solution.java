4d2fd516ce15e4e5391a6336b34bca975887d7fc
heshan <5373827@qq.com>
2024-01-20 17:54:29 +0800

    p116

---
+++ src/main/java/leetcode/p116/Solution.java
@@ -0,0 +1,63 @@
+package leetcode.p116;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/20
+ */
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+
+public class Solution {
+
+    public Node connect(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null) {
+            queue.add(root);
+        }
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            Node prev = null;
+            for (int i = 0; i < size; i++) {
+                Node node = queue.remove();
+                if (prev != null) {
+                    prev.next = node;
+                }
+                prev = node;
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+
+        return root;
+    }
+}
