9d6f4b0e7c8589d84165eaae03becf0043e90243
heshan <5373827@qq.com>
2024-01-21 06:38:04 +0800

    p125

---
+++ src/main/java/leetcode/p125/Solution.java
@@ -0,0 +1,46 @@
+package leetcode.p125;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/21
+ */
+public class Solution {
+
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            while (l < r && !isValid(s.charAt(l))) {
+                l++;
+            }
+            while (l < r && !isValid(s.charAt(r))) {
+                r--;
+            }
+
+            char c1 = Character.toLowerCase(s.charAt(l++));
+            char c2 = Character.toLowerCase(s.charAt(r--));
+
+            if (c1 != c2) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isValid(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return true;
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            return true;
+        }
+        if (ch >= 'A' && ch <= 'Z') {
+            return true;
+        }
+
+        return false;
+    }
+}
