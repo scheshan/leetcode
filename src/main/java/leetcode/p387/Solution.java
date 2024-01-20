77afbeeb41ddf40b93ddd1c32a747537a0f49907
heshan <5373827@qq.com>
2024-01-20 20:20:41 +0800

    p387

---
+++ src/main/java/leetcode/p387/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p387;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/20
+ */
+public class Solution {
+
+    public int firstUniqChar(String s) {
+        int[] cnt = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (cnt[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
