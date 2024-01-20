2404be73c19d539d28bfebadae78b6468bbb35a3
heshan <5373827@qq.com>
2024-01-20 20:02:13 +0800

    p217

---
+++ src/main/java/leetcode/p217/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/20
+ */
+public class Solution {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> exist = new HashSet<>();
+        for (int num : nums) {
+            if (exist.contains(num)) {
+                return true;
+            }
+
+            exist.add(num);
+        }
+
+        return false;
+    }
+}
