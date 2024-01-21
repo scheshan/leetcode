c96d417ab4199cf942b31ddd347fc6876ec0adbf
heshan <5373827@qq.com>
2024-01-21 11:39:14 +0800

    p111

---
+++ src/main/java/leetcode/p111/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.p111;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/21
+ */
+public class Solution {
+
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        } else if (root.left == null) {
+            return minDepth(root.right) + 1;
+        } else if (root.right == null) {
+            return minDepth(root.left) + 1;
+        } else {
+            return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+        }
+    }
+}
