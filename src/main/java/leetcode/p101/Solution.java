edc4148459225b088e42b5d648852c63d78ddf6f
heshan <5373827@qq.com>
2024-01-21 19:59:33 +0800

    p101

---
+++ src/main/java/leetcode/p101/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p101;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/21
+ */
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        return isSymmetric(root, root);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left != null && right != null) {
+            return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+        } else {
+            return false;
+        }
+    }
+}
