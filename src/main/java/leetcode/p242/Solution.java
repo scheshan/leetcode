1847fdd6b8da055b430e6df3737eb45df14d5386
heshan <5373827@qq.com>
2024-01-21 10:59:21 +0800

    p242

---
+++ src/main/java/leetcode/p242/Solution.java
@@ -0,0 +1,30 @@
+package leetcode.p242;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/21
+ */
+public class Solution {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            cnt[t.charAt(i) - 'a']--;
+            if (cnt[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
