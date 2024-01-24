e8f74730da234b493286ed1f116a90e740db5ae0
heshan <5373827@qq.com>
2024-01-24 13:56:24 +0800

    p200

---
+++ src/main/java/leetcode/p200/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.p200;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/24
+ */
+public class Solution {
+
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] != '1') {
+            return;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j - 1);
+        dfs(grid, i, j + 1);
+    }
+}
