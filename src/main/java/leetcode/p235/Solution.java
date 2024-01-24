49a46f7d7fbcae63309977435e2e5ea426bbde58
heshan <5373827@qq.com>
2024-01-24 13:18:22 +0800

    p235

---
+++ src/main/java/leetcode/p235/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p235;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/24
+ */
+public class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
+    }
+}
