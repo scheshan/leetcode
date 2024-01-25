4f0cb7fdf844cd8ea97c9024cf33fdb70401d553
heshan <heshan@baicizhan.com>
2024-01-25 10:56:54 +0800

    p189

---
+++ src/main/java/leetcode/p189/Solution.java
@@ -0,0 +1,32 @@
+package leetcode.p189;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/25
+ */
+public class Solution {
+
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        if (k == 0) {
+            return;
+        }
+
+        rotate(nums, 0, nums.length - 1);
+        rotate(nums, 0, k - 1);
+        rotate(nums, k, nums.length - 1);
+    }
+
+    private void rotate(int[] nums, int l, int r) {
+        while (l < r) {
+            int d = nums[l];
+            nums[l] = nums[r];
+            nums[r] = d;
+
+            l++;
+            r--;
+        }
+    }
+}
