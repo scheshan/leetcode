363a52294c39aa0f25b3f40fa6eda2c478b483d4
heshan <5373827@qq.com>
2024-01-26 14:18:55 +0800

    p122

---
+++ src/main/java/leetcode/p122/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.p122;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/26
+ */
+public class Solution {
+
+    public int maxProfit(int[] prices) {
+        int have = -prices[0], notHave = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int h = Math.max(have, notHave - prices[i]);
+            int nh = Math.max(notHave, have + prices[i]);
+
+            have = h;
+            notHave = nh;
+        }
+
+        return Math.max(have, notHave);
+    }
+}
