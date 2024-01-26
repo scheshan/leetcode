de4823758c8cd48484ea96c56d5bfe1645ce4858
heshan <5373827@qq.com>
2024-01-26 20:22:13 +0800

    p1035

---
+++ src/main/java/leetcode/p1035/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p1035;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/26
+ */
+public class Solution {
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length + 1][nums2.length + 1];
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+
+        return dp[nums1.length][nums2.length];
+    }
+}
