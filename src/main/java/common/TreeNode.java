f69e5110aaf6558e59a56ec9aed6ebab459df2e7
heshan <5373827@qq.com>
2024-01-28 16:49:05 +0800

    p104

---
+++ src/main/java/common/TreeNode.java
@@ -0,0 +1,26 @@
+package common;
+
+/**
+ * TreeNode
+ *
+ * @author heshan
+ * @date 2024/1/28
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ src/main/java/leetcode/p104/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.p104;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/28
+ */
+public class Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
