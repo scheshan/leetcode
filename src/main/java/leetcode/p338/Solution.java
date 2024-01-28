c215861e1cc6934a937f133bd3852c8f14203c24
heshan <5373827@qq.com>
2024-01-29 07:24:01 +0800

    p338

---
+++ src/main/java/leetcode/p338/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.p338;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/29
+ */
+public class Solution {
+
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        if (n == 0) {
+            return res;
+        }
+
+        res[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+
+        return res;
+    }
+}
