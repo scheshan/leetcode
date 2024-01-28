fee264cbf326d9be4dc02ec332209d00744cb707
heshan <5373827@qq.com>
2024-01-28 18:48:03 +0800

    p1

---
+++ src/main/java/leetcode/p1/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.p1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/28
+ */
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int d = target - n;
+
+            if (map.containsKey(d)) {
+                return new int[]{i, map.get(d)};
+            }
+
+            map.put(n, i);
+        }
+
+        return null;
+    }
+}
