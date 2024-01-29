e43825a9788e818b4a29b94a132763627f682cc5
heshan <heshan@baicizhan.com>
2024-01-29 11:14:03 +0800

    p374

---
+++ src/main/java/leetcode/p374/GuessGame.java
@@ -0,0 +1,14 @@
+package leetcode.p374;
+
+/**
+ * GuessGame
+ *
+ * @author heshan
+ * @date 2024/1/29
+ */
+public class GuessGame {
+
+    public int guess(int num) {
+        return 0;
+    }
+}
