b450fb3e4bffac6de776c1c468f45d5209af6bdb
heshan <5373827@qq.com>
2024-01-29 16:13:00 +0800

    p114

---
+++ src/main/java/leetcode/p114/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.p114;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/29
+ */
+public class Solution {
+
+    private TreeNode pre;
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = null;
+
+        if (pre != null) {
+            pre.right = root;
+        }
+        pre = root;
+
+        flatten(left);
+        flatten(right);
+    }
+}
