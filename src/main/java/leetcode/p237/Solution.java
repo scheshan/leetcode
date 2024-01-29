b94e4221b6849d36c1e65fc91f429c4571e41b37
heshan <5373827@qq.com>
2024-01-29 15:30:29 +0800

    p237

---
+++ src/main/java/leetcode/p237/Solution.java
@@ -0,0 +1,17 @@
+package leetcode.p237;
+
+import common.ListNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/29
+ */
+public class Solution {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
