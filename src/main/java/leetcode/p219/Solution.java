96bc5580b4e5f037e336e91248fe9607642ce5de
heshan <5373827@qq.com>
2024-01-29 19:51:12 +0800

    p219

---
+++ src/main/java/leetcode/p219/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.p219;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/29
+ */
+public class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+
+            map.put(num, i);
+        }
+
+        return false;
+    }
+}
