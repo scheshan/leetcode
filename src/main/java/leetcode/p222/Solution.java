6647c82e9b98c7a861cda6156822ec5f0bc8108c
heshan <5373827@qq.com>
2024-01-29 20:00:46 +0800

    p222

---
+++ src/main/java/leetcode/p222/Solution.java
@@ -0,0 +1,20 @@
+package leetcode.p222;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/29
+ */
+public class Solution {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
