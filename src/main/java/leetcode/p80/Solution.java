9d1a2b45cd3dc3a95ddb62d866f1a341d2e4300f
heshan <5373827@qq.com>
2024-01-29 19:28:17 +0800

    p80

---
+++ src/main/java/leetcode/p80/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.p80;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/29
+ */
+public class Solution {
+
+    public int removeDuplicates(int[] nums) {
+        int slow = 0, fast = 0;
+        while (fast < nums.length) {
+            if (slow < 2 || nums[fast] != nums[slow - 2]) {
+                nums[slow++] = nums[fast];
+            }
+            fast++;
+        }
+
+        return slow;
+    }
+}
