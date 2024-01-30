348fc8654bbaf9fb5ae20045d64312502d789154
heshan <5373827@qq.com>
2024-01-30 16:26:19 +0800

    p98

---
+++ src/main/java/leetcode/p98/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.p98;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/30
+ */
+public class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean valid(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        return valid(node.left, min, node.val) && valid(node.right, node.val, max);
+    }
+}
