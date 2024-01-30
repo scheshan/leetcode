1201b7664a0a1a7f2a361923aa4d125e4e32dd2e
heshan <5373827@qq.com>
2024-01-30 16:32:26 +0800

    p108

---
+++ src/main/java/leetcode/p108/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.p108;
+
+import common.TreeNode;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/1/30
+ */
+public class Solution {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length);
+    }
+
+    private TreeNode build(int[] nums, int l, int r) {
+        if (l >= r) {
+            return null;
+        }
+
+        int mid = l + ((r - l) >> 1);
+        TreeNode res = new TreeNode(nums[mid]);
+        res.left = build(nums, l, mid);
+        res.right = build(nums, mid + 1, r);
+        return res;
+    }
+}
