cea402336480d84d5a0c538e1236530e29e8e34d
heshan <5373827@qq.com>
2024-02-04 13:35:12 +0800

    p292

---
+++ src/main/java/leetcode/p292/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.p292;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/2/4
+ */
+public class Solution {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
