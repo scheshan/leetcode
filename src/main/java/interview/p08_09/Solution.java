7cd9ad46131cb3176a168225360fefd8998fc23e
heshan <5373827@qq.com>
2024-02-19 20:50:56 +0800

    p08_09

---
+++ src/main/java/interview/p08_09/Solution.java
@@ -0,0 +1,39 @@
+package interview.p08_09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Solution
+ *
+ * @author heshan
+ * @date 2024/2/19
+ */
+public class Solution {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        backtracking(0, 0, n, res, sb);
+
+        return res;
+    }
+
+    private void backtracking(int left, int right, int n, List<String> res, StringBuilder sb) {
+        if (sb.length() == n << 1) {
+            res.add(sb.toString());
+            return;
+        }
+
+        if (left < n) {
+            sb.append('(');
+            backtracking(left + 1, right, n, res, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (left > right) {
+            sb.append(')');
+            backtracking(left, right + 1, n, res, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
