e842ff4bf8911fcd07eaf50b1aff60dee9be82df
scheshan <5373827@qq.com>
2026-03-31 07:43:47 +0800

    p206

---
+++ src/main/java/leetcode/p206/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.p206;
+
+import common.ListNode;
+
+public class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode res = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = res;
+            res = head;
+            head = next;
+        }
+
+        return res;
+    }
+}
