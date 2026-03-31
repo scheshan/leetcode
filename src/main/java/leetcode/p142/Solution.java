278b07d69077492dc5a1c392fff786c14749f4a6
scheshan <5373827@qq.com>
2026-03-31 10:19:41 +0800

    p142

---
+++ src/main/java/leetcode/p142/Solution.java
@@ -0,0 +1,28 @@
+package leetcode.p142;
+
+import common.ListNode;
+
+public class Solution {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                fast = slow;
+                slow = head;
+
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+    }
+}
