d93bd6aec9090c1d03ea0f79ef37b57c99434dc2
scheshan <5373827@qq.com>
2026-03-31 10:21:30 +0800

    p21

---
+++ src/main/java/leetcode/p21/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.p21;
+
+import common.ListNode;
+
+public class Solution {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                tail = tail.next;
+                list1 = list1.next;
+            } else {
+                tail.next = list2;
+                tail = tail.next;
+                list2 = list2.next;
+            }
+        }
+
+        if (list1 != null) {
+            tail.next = list1;
+        } else {
+            tail.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
